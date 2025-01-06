f6235330d02738bbb6bac8c2e5c8b41b42e092d4
coderjcronin <coderjmc@gmail.com>
2025-01-06 15:36:37 -0500

    Add frame clock and use to regulate main loop

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ def main ():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     pygame.init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    frames_clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +21,7 @@ def main ():
                 return
         pygame.Surface.fill(screen, (0,0,0))
         pygame.display.flip()
+        dt = (frames_clock.tick(60)/1000)
 
 if __name__ == "__main__":
     main()
